168e2c3a216b86dd66164094cd256a2046f95848
Achin-Agarwal <achinagarwal9044@gmail.com>
2025-10-14 18:58:48 +0530

    fix: Remove unused connectMongo function call from index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,6 @@ app.use();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectMongo();
 
 app.use((err, req, res, next) => {
   console.error("Error encountered:", err);
