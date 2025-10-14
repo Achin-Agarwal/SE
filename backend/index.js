39b384deb4e88f0f928a8718587b9753dbca7370
Achin-Agarwal <achinagarwal9044@gmail.com>
2025-10-14 19:08:14 +0530

    fix: Add missing cors middleware initialization in index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     console.log(req.url, req.method);
     next();
 })
-app.use();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
