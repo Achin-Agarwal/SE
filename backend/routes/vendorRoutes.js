5f80f6ac634800b184ed48eb5acf3766703f8c12
Achin-Agarwal <achinagarwal9044@gmail.com>
2025-11-07 20:28:04 +0530

    fix: Remove redundant logging and add specific logging for profile image during vendor registration

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -56,7 +56,6 @@ router.post(
   safeHandler(async (req, res) => {
     try {
       let parsedBody = req.body;
-      console.log("FIELDS:", req.body);
       console.log("FILES:", req.files);
       if (typeof parsedBody.location === "string") {
         try {
@@ -79,8 +78,8 @@ router.post(
         );
       }
       const hashedPassword = await bcrypt.hash(password, 10);
+      console.log("Profile Image File:", req.files?.profileImage);
       let profileImageUrl = null;
-      console.log(req.files?.profileImage);
       if (req.files?.profileImage) {
         profileImageUrl = req.files.profileImage[0].location;
         if (
